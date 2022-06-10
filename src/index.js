6ecd7d07420428dcf63bb6c3143f508df12c54ea
Mihailo Igic <masaale.ma@gmail.com>
2022-06-10 22:53:16 +0200

    install redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import rootReducer from './redux/reducers';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from "react-router-dom";
 import GetRoutes from './routes';
@@ -8,13 +10,15 @@ import Header from './view/header';
 import Footer from './view/footer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const store = configureStore({ reducer: rootReducer });
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Header/>
-      <GetRoutes/>
-      <Footer/>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Header/>
+        <GetRoutes/>
+        <Footer/>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
-
